69966c362599dcbf678eb156539370450ce6cb4f
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-08-03 20:40:21 +0300

    main: extracted get_canonical_school_name function

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ import difflib
 import re
 
 re_multiple_spaces = re.compile(r'''\s+''')
+re_grup_scolar = re.compile(r'''GRUP(UL)? SCOLAR ''')
+re_colegiu = re.compile(r'''COLEGIU(L)? (NATIONAL)? (DE)?''')
+re_liceu = re.compile(r'''LICEU(L)? (TEORETIC)?''')
 
 with open('/home/ciupicri/altii/irina/evolutie_licee.csv', 'rt') as f:
     csv_reader = csv.DictReader(f, delimiter=';')
     licee = [i for i in csv_reader]
 
+def get_canonical_school_name(s):
+    return \
+        re_liceu.sub('',
+            re_colegiu.sub('',
+                re_grup_scolar.sub('', 
+                    re_multiple_spaces.sub(' ', s)))).strip()
+
 data = {}
 for i in licee:
     data.setdefault(i['judet'], []).append(i)
     del i['judet']
-    i['scoala_canonica'] = re_multiple_spaces.sub(' ', i['scoala'])\
-            .replace('GRUP SCOLAR', '')\
-            .replace('GRUPUL SCOLAR', '')\
-            .replace('COLEGIU NATIONAL', '')\
-            .replace('COLEGIUL NATIONAL', '')\
-            .replace('COLEGIUL', '')\
-            .replace('LICEUL TEORETIC', '')\
-            .replace('LICEUL', '')
+    i['scoala_canonica'] = get_canonical_school_name(i['scoala'])
 
 del licee
